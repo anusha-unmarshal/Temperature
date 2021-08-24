ced1a2a2169a2d4da4c5801c079a73e5990b70e0
Anusha Sridhar <anusha.unmarshal.io>
2021-08-24 13:18:39 +0530

    Send notification to owner functionality

--- ParkingLot.js
+++ ParkingLot.js
@@ -1,11 +1,64 @@
-export class ParkingLot {
+class ParkingLot {
     parking = [];
+    availableSlots;
+    owner;
 
-    park(vehicle) {
-        return this.parking.push(vehicle);
+    constructor(availableSlots, owner) {
+        this.availableSlots = availableSlots;
+        this.owner = owner;
+    }
+    park(vehicle){
+        if (!this.isFull()) {
+            this.parking.push(vehicle);
+            this.availableSlots -= 1;
+        }
+        if (this.isFull()){
+           this.owner.isFull();
+        }
     }
 
-    unpark() {
+    unpark(vehicle) {
+        if (this.isParked(vehicle)) {
+            this.parking = this.parking.filter((item) => {
+                return item !== vehicle;
+            });
+            this.availableSlots += 1
+            if (this.availableSlots === 1){
+                this.owner.isAvailable()
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
+    isParked(vehicle) {
+        for (let i=0; i<this.parking.length;i++){
+            if (this.parking[i] === vehicle){
+                return true
+            }
+        }
+        return false
+    }
+
+    isFull() {
+       return this.availableSlots === 0;
     }
 }
+export class Vehicle {
+
+}
+
+class Owner {
+    isLotFull = false;
+
+    isFull() {
+        this.isLotFull = true;
+    }
+
+    isAvailable() {
+        this.isLotFull = false;
+    }
+}
+export {ParkingLot, Owner};
--- ParkingLot.test.js
+++ ParkingLot.test.js
@@ -1,16 +1,61 @@
-const {ParkingLot} = require("./ParkingLot")
+const {Vehicle, ParkingLot, Owner} = require("./ParkingLot")
 
 describe("Parking lot", () => {
-    const parking = new ParkingLot();
+    const owner = new Owner();
+    const parkingLot = new ParkingLot(1, owner);
     test("Park a vehicle", () => {
-        const vehicle = "KA01MD1234";
+        const vehicle = new Vehicle();
 
-        expect(parking.park(vehicle)).toBeTruthy();
+        parkingLot.park(vehicle);
+
+        expect(parkingLot.isParked(vehicle)).toBe(true);
     });
 
-    test("Unpark a vehicle", () => {
-        const vehicle = parking.unpark();
+    test("Un-park a vehicle should not work without parking it first", () => {
+        const vehicle = new Vehicle();
 
-        expect(vehicle).not.toBe(null);
+        const unparked = parkingLot.unpark(vehicle);
+
+        expect(unparked).toBeFalsy();
+    });
+
+    test("Park and un-park a vehicle", () => {
+        const vehicle = new Vehicle();
+
+        parkingLot.park(vehicle);
+        const unparked = parkingLot.unpark(vehicle);
+
+        expect(unparked).toBeTruthy();
+    });
+
+    test("Cannot park when lot is full", () => {
+        const vehicle = new Vehicle();
+
+        parkingLot.park(vehicle);
+
+        expect(parkingLot.isParked(vehicle)).toBeFalsy();
+    })
+
+    test("Check if parking lot is full", () => {
+        const isFull = parkingLot.isFull();
+
+        expect(isFull).toBeTruthy();
+    });
+
+    test("Notify the owner when lot is full", () => {
+        const vehicle = new Vehicle();
+
+        parkingLot.park(vehicle);
+
+        expect(owner.isLotFull).toBeTruthy();
+    });
+
+    test("Notify owner when parking slot is available", () => {
+        const vehicle = new Vehicle();
+
+        parkingLot.park(vehicle);
+        parkingLot.unpark(vehicle);
+
+        expect(owner.isLotFull).toBeFalsy();
     })
 });
